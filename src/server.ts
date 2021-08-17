c29bd12e2dd6e63065e2937b31f2ca9e5dae0393
piann <shadow_hat@naver.com>
2021-08-17 20:54:50 +0900

    init websocket

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,23 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
 app.set('view engine', "pug");
-app.set('views', __dirname + "/views")
-app.use('/public', express.static(__dirname + "public"))
+app.set('views', __dirname + "/views");
+app.use('/public', express.static(__dirname + "/public"));
+app.get("/" , (req, res)=> res.render("home"));
+app.get("/*" , (req, res)=> res.redirect("/"));
 
-app.get("/" , (req, res)=> res.render("home"))
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`)
 
-app.listen(3000, handleListen);
+const handleConnect = (socket:WebSocket) => console.log(socket)
+
+const httpServer = http.createServer(app);
+const wsServer = new WebSocket.Server({ server : httpServer });
+
+wsServer.on("connection", handleConnect)
+
+httpServer.listen(3000, handleListen);
